24c0b70e5977cc26803c779670380533c5bd8d47
d5ng <dong5ffice@gmail.com>
2026-01-30 00:45:48 +0900

    feat: integrate TypeORM with PostgreSQL and define Post entity structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PostModule } from './post/post.module'
 
 @Module({
-  imports: [PostModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'test',
+      password: process.env.DB_PASSWORD || 'test',
+      database: process.env.DB_DATABASE || 'inflearn',
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV === 'development' ? true : false,
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,6 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity({
+  name: 'posts',
+})
 export class Post {
+  @PrimaryGeneratedColumn({ type: 'int' })
   id: number
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
   title: string
+
+  @Column({ type: 'text', nullable: false })
   content: string
+
+  @Column({ type: 'int', nullable: false, name: 'author_id' })
   authorId: number
 }
