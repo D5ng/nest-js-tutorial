af14d069542d1b41812612ad0d7549a1accae8f4
d5ng <dong5ffice@gmail.com>
2026-02-05 14:43:52 +0900

    feat: implement user profile management endpoints in UsersController with access control

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,63 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Put, Req, UnauthorizedException, UseGuards } from '@nestjs/common'
+import { AccessTokenGuard } from 'src/common/guards/access-token.guard'
+import type { RequestWithUser } from 'src/common/types/request-with-user.type'
+import { UpdateUserDto } from './dto/update-user-dto'
+import { User } from './users.entity'
+import { UsersService } from './users.service'
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(AccessTokenGuard)
+  @Get('me')
+  async getMe(@Req() req: RequestWithUser) {
+    const userId = req.user['sub']
+
+    if (!userId) {
+      throw new UnauthorizedException('접근 권한이 없습니다.')
+    }
+
+    const { id, name, username } = await this.usersService.findById(userId)
+
+    return {
+      id,
+      name,
+      username,
+    }
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Put('me')
+  async updateUser(@Req() req: RequestWithUser, @Body() updateUserDto: UpdateUserDto) {
+    const userId = req.user['sub']
+
+    if (!userId) {
+      throw new UnauthorizedException('접근 권한이 없습니다.')
+    }
+
+    const user = await this.usersService.update(userId, updateUserDto)
+
+    return this.shieldUserInfo(user)
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Delete('me')
+  async deleteUser(@Req() req: RequestWithUser) {
+    const userId = req.user['sub']
+
+    if (!userId) {
+      throw new UnauthorizedException('접근 권한이 없습니다.')
+    }
+
+    return this.usersService.remove(userId)
+  }
+
+  private shieldUserInfo(user: User) {
+    return {
+      ...user,
+      password: undefined,
+      refreshToken: undefined,
+    }
+  }
+}
