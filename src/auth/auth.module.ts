bad09e2982ea0386c85df001515534e7ff14d212
d5ng <dong5ffice@gmail.com>
2026-02-05 14:44:37 +0900

    feat: create AuthModule to encapsulate authentication logic with JWT and user management

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { UsersModule } from 'src/users/users.module'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { AccessTokenStrategy } from './strategies/access-token.strategy'
+import { RefreshTokenStrategy } from './strategies/refresh-token.strategy'
+
+@Module({
+  imports: [UsersModule, JwtModule.register({})],
+  providers: [AuthService, AccessTokenStrategy, RefreshTokenStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
